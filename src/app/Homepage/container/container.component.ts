cda8b4bd7d266602f0d71cdcbd8f0d7b44bed8f2
Ashutosh <200050015@iitb.ac.in>
2021-06-03 20:46:59 +0530

    5commit

--- src/app/Homepage/container/container.component.ts
+++ src/app/Homepage/container/container.component.ts
@@ -7,9 +7,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContainerComponent implements OnInit {
 
-  constructor() { }
+  cards:carddetails[]
+
+  constructor() { 
+    this.cards=[
+      {
+        title:"Merge PDF",
+        c:1,
+        desc:"merges two or more pdf",
+        classname:"merge-pdf"
+
+      },
+      {
+        title:"Split PDF",
+        c:2,
+        desc:"merges two or more pdf",
+        classname:"merge-pdf"
+
+      },
+      {
+        title:"Convert to PDF",
+        c:3,
+        desc:"merges two or more pdf",
+        classname:"merge-pdf"
+
+      },
+      {
+        title:"Edit PDF",
+        c:4,
+        desc:"merges two or more pdf",
+        classname:"merge-pdf"
+
+      }
+    ]
+  }
 
   ngOnInit(): void {
   }
 
+}
+
+class carddetails {
+
+  title:string
+  c:number
+  desc:string
+  classname:string
+  // constructor(parameters) {
+    
+  // }
 }
