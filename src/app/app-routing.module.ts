8039a7492745500b638f14c2dc9afab1fe01d3e0
Yashsaraogi <Yashsaraogi67@gmail.com>
2021-06-07 19:54:19 +0530

    final1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { ConverttopdfPreviewComponent } from './converttopdf-preview/converttopdf-preview.component';
+import { EditPDFPreviewComponent } from './edit-pdfpreview/edit-pdfpreview.component';
+import { MergePreviewComponent } from './merge-preview/merge-preview.component';
+import { PreviewComponent } from './preview/preview.component';
+import { SplitPreviewComponent } from './split-preview/split-preview.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'merge',component: MergePreviewComponent },
+  {path: 'split',component: SplitPreviewComponent },
+  {path: 'preview',component: PreviewComponent },
+  {path: 'Homepage',component: AppComponent },
+  {path: 'edit',component: EditPDFPreviewComponent },
+  {path: 'convert',component: ConverttopdfPreviewComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
